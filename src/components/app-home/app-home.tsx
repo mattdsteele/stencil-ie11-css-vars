c2343e64421b6f838962961798d86afa522a39e7
mattdsteele <orphum@gmail.com>
2018-01-04 12:13:12 -0600

    Demonstrate IE11 stye failure

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,26 +1,22 @@
 import { Component } from '@stencil/core';
 
-
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.scss'
 })
 export class AppHome {
-
   render() {
     return (
       <div>
+        <div class="logo" />
         <p>
-          Welcome to the Stencil App Starter.
-          You can use this starter to build entire apps all with
-          web components using Stencil!
-          Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
+          Welcome to the Stencil App Starter. You can use this starter to build
+          entire apps all with web components using Stencil! Check out our docs
+          on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
         </p>
 
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
+        <stencil-route-link url="/profile/stencil">
+          <button>Profile page</button>
         </stencil-route-link>
       </div>
     );
